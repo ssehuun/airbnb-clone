c3a3a3641fc6c11026abe95f74928cc31c2c9f70
ssehuun <shn413@naver.com>
2021-03-28 19:45:45 +0900

    10.0 Introduction Urls and Views, #10.1 HttpResponse and Render

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def all_rooms(request):
+    # print(dir(request))
+    return render(request, "all_rooms")
