bcb6874d0f68e92d741eb405450ee709f2fe2f69
ssehuun <shn413@naver.com>
2021-04-01 13:32:24 +0900

    #11.4 Using Django Paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,25 +1,19 @@
 from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 # Create your views here.
 
 
 def all_rooms(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)  # page 숫자가 입력되지 않았을때 1페이지로 설정
-    content_size = 10
-    limit = content_size * page
-    offset = limit - content_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / content_size)
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    # print(vars(rooms))
     return render(
         request,
         "rooms/home.html",
-        {
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
-        },
+        {"rooms": rooms},
     )
