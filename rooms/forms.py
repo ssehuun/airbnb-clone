5f8bfcd1147e4f419a95215b0610c2df9090b198
ssehuun <shn413@naver.com>
2021-05-23 17:50:40 +0900

    #13.9 Forms are Awesome

--- rooms/forms.py
+++ rooms/forms.py
@@ -8,7 +8,7 @@ class SearchForm(forms.Form):
     city = forms.CharField(initial="Anywhere")
     country = CountryField(default="KR").formfield()
     room_type = forms.ModelChoiceField(
-        empty_label="Any kind", queryset=models.RoomType.objects.all()
+        required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
     )
     price = forms.IntegerField(required=False)
     guests = forms.IntegerField(required=False)
@@ -18,8 +18,12 @@ class SearchForm(forms.Form):
     instant_book = forms.BooleanField(required=False)
     superhost = forms.BooleanField(required=False)
     amenities = forms.ModelMultipleChoiceField(
-        queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple
+        required=False,
+        queryset=models.Amenity.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
     )
     facilities = forms.ModelMultipleChoiceField(
-        queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple
+        required=False,
+        queryset=models.Facility.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
     )
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from django_countries import countries
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.http import Http404
 from django.urls import reverse
 from django.shortcuts import render, redirect
@@ -29,6 +29,66 @@ class RoomDetail(DetailView):
     pk_url_kwarg = "potato"  # url에서 pk로 던져주는 키워드를 potato를 바꿔도 됨(url.py에서 potato 변경요)
 
 
-def search(request):
-    form = forms.SearchForm
-    return render(request, "rooms/search.html", {"form": form})
+class SearchView(View):
+    """ SearchView Definition """
+
+    def get(self, request):
+        country = request.GET.get("country")
+
+        if country:
+            form = forms.SearchForm(request.GET)  # 어떤걸 체크했는지 search후 form에 기억하게 하고 싶음
+            if form.is_valid():
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+
+                filter_args = {}
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city
+
+                filter_args["country"] = country
+
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
+
+                if price is not None:
+                    filter_args["price__lte"] = price
+
+                if guests is not None:
+                    filter_args["guests__gte"] = guests
+
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
+
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
+
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
+
+                if instant_book is True:
+                    filter_args["instant_book"] = True
+
+                if superhost is True:
+                    filter_args["host__superhost"] = True
+
+                for amenity in amenities:
+                    filter_args["amenities"] = amenity
+
+                for facility in facilities:
+                    filter_args["facilities"] = facility
+
+                rooms = models.Room.objects.filter(**filter_args)
+        else:
+            form = forms.SearchForm()
+
+        return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
