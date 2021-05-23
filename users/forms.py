7863b20d1be10af6a1bd6701c947db4a96d10789
ssehuun <shn413@naver.com>
2021-05-23 22:13:03 +0900

    14.2 Validating Email

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,19 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_email(self):  # data를 확인하고 싶으면 clean_data 형식으로 사용
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    def clean_password(self):
+        return "3333"
